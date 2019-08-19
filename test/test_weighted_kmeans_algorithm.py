acc1b026fce6e701b96afc4c00f27aa7ebbe4c65
Emrah Cimren <cimren.1@gmail.com>
2019-08-18 22:22:52 -0700

    Updated test

--- test/test_weighted_kmeans_algorithm.py
+++ test/test_weighted_kmeans_algorithm.py
@@ -6,7 +6,7 @@ import unittest
 import pandas as pd
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
 from src import weighted_kmeans_algorithm as wkm
 
 
